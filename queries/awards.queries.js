e488e16f860dd02d549b709b87a9f7ff67682fc3
Spencer Rose <Spencer.Rose@gov.bc.ca>
2023-04-20 17:16:33 -0700

    Updates to fix discrepancies in reports.

--- queries/awards.queries.js
+++ queries/awards.queries.js
@@ -49,14 +49,44 @@ const awardsQueries = {
             filterClauses.push('quantity != 0');
         }
 
+        // return {
+        //     sql: `SELECT * FROM awards
+        //         ${filterClauses.length > 0 ? ' WHERE ' + filterClauses.join(' AND ') : ''}
+        //         ${orderClause}
+        //         ${limitClause}
+        //           OFFSET ${offset};`,
+        //     data: [],
+        // };
+
         return {
-            sql: `SELECT * FROM awards 
-                ${filterClauses.length > 0 ? ' WHERE ' + filterClauses.join(' AND ') : ''}
-                ${orderClause}
-                ${limitClause}
-                  OFFSET ${offset};`,
-            data: [],
-        };
+            sql: `
+            SELECT * 
+            FROM awards AS "awds"
+                    -- award options details
+                     LEFT JOIN (
+                      SELECT awdopts.award as opts_award_id,
+                             JSON_AGG( json_build_object(
+                                 'id', awdopts.id,
+                                 'award', awdopts.award,
+                                 'type', awdopts.type,
+                                 'name', awdopts.name,
+                                 'description', awdopts.description,
+                                 'label', awdopts.label,
+                                 'value', awdopts.value,
+                                 'customizable', awdopts.customizable
+                            )) AS options
+                      FROM "award_options" AS "awdopts"
+                      GROUP BY opts_award_id
+                ) AS "opts" ON opts_award_id = "awds"."id"
+                -- recipient award selections
+                LEFT JOIN (
+                    SELECT awdsel.award as select_award_id, COUNT(*) AS selected
+                    FROM "award_selections" AS "awdsel"
+                    GROUP BY awdsel.award
+                ) AS "selections" ON select_award_id = "awds"."id"  
+            ;`,
+                data: []
+        }
 
     },
     findByFields: (fields, values, schema, sort) => {
@@ -97,6 +127,6 @@ exports.queries = awardsQueries;
  * @public
  */
 
-exports.findAll = async (filter, schema) => {
-    return await query(awardsQueries.findAll(filter, schema));
+exports.findAll = async (filter) => {
+    return await query(awardsQueries.findAll(filter));
 }
