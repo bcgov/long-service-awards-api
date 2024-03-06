cb00a3ea682e223a9934f80940264a72a571adf5
DenisKorkut-bcgov <125493524+DenisKorkut-bcgov@users.noreply.github.com>
2024-03-05 16:16:06 -0800

    Update password reset query to use lower case

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,9 +5,13 @@
  * MIT Licensed
  */
 
-const User = require('../models/users.model');
-const {resetToken, deleteToken, validateToken} = require('../services/cache.services');
-const {sendResetPassword} = require("../services/mail.services");
+const User = require("../models/users.model");
+const {
+  resetToken,
+  deleteToken,
+  validateToken,
+} = require("../services/cache.services");
+const { sendResetPassword } = require("../services/mail.services");
 
 /**
  * Get current user state retrieved from IDIR (SAML) authentication
@@ -34,8 +38,8 @@ exports.info = async (req, res, next) => {
           last_name: res.locals.user.last_name,
           role: res.locals.user.role,
           organizations: res.locals.user.organizations,
-          authenticated: true
-        }
+          authenticated: true,
+        },
       });
     }
     // restricted user authorization
@@ -47,13 +51,12 @@ exports.info = async (req, res, next) => {
           guid: res.locals.user.guid,
           idir: res.locals.user.idir,
           role: res.locals.user.role,
-          authenticated: false
-        }
+          authenticated: false,
+        },
       });
-    }
-    else {
-      if (req.url.match('^\/(rsvp)\/[^\/]+\/[^\/]+$')) return res.status(200);
-      return next(new Error('noAuth'));
+    } else {
+      if (req.url.match("^/(rsvp)/[^/]+/[^/]+$")) return res.status(200);
+      return next(new Error("noAuth"));
     }
   } catch (err) {
     return next(err);
@@ -71,24 +74,22 @@ exports.info = async (req, res, next) => {
  */
 
 exports.login = async (req, res, next) => {
-
   // Explicitly save the session before returning
   req.session.save(() => {
     if (req.isAuthenticated()) {
       res.status(200).json({
         message: {
-          severity: 'success',
-          summary: 'Welcome',
-          detail: 'You are now signed in.'
+          severity: "success",
+          summary: "Welcome",
+          detail: "You are now signed in.",
         },
         result: req.user,
       });
-    }
-    else {
-      next(new Error('noAuth'));
+    } else {
+      next(new Error("noAuth"));
     }
   });
-}
+};
 
 /**
  * User sign out.
@@ -117,13 +118,15 @@ exports.logout = async (req, res, next) => {
     });
 
     // Passport session logout
-    req.logout(function(err) {
-      if (err) { return next(err); }
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
       res.status(200).json({
         message: {
-          severity: 'success',
-          summary: 'Goodbye!',
-          detail: 'You are now signed out.'
+          severity: "success",
+          summary: "Goodbye!",
+          detail: "You are now signed out.",
         },
         result: {},
       });
@@ -145,15 +148,14 @@ exports.logout = async (req, res, next) => {
 
 exports.validateToken = async (req, res, next) => {
   try {
-
     // destructure reset token
-    const {userid, token} = req.params;
+    const { userid, token } = req.params;
 
     // find user by ID
     const user = await User.findById(userid);
 
     // check if user is a registered user
-    if (!user) return next(new Error('notFound'));
+    if (!user) return next(new Error("notFound"));
 
     // check if token is valid
     const isValid = await validateToken(userid, token);
@@ -162,12 +164,11 @@ exports.validateToken = async (req, res, next) => {
       message: {},
       result: isValid,
     });
+  } catch (e) {
+    console.error(e);
+    next(new Error("passwordResetFailed"));
   }
-  catch (e) {
-    console.error(e)
-    next(new Error('passwordResetFailed'));
-  }
-}
+};
 
 /**
  * User request for password reset
@@ -180,17 +181,16 @@ exports.validateToken = async (req, res, next) => {
  */
 
 exports.requestResetPassword = async (req, res, next) => {
-
   // find user by email
   const { email } = req.body || {};
-  const user = await User.findByEmail(email);
+  const user = await User.findByEmail(email.toLowerCase());
   const expiry = 60 * 60;
 
   // check if user is a registered user
-  if (!user) return next(new Error('notFound'));
+  if (!user) return next(new Error("notFound"));
 
   // get user ID
-  const {id} = user.data;
+  const { id } = user.data;
 
   // generate new token
   const token = await resetToken(user.id, expiry);
@@ -198,18 +198,18 @@ exports.requestResetPassword = async (req, res, next) => {
   // send reset link in email to user
   const response = await sendResetPassword({
     email,
-    link: `${process.env.LSA_APPS_ADMIN_URL}/reset-password/${id}/${token}`
+    link: `${process.env.LSA_APPS_ADMIN_URL}/reset-password/${id}/${token}`,
   });
 
   res.status(200).json({
     message: {
-      severity: 'success',
-      summary: 'Password Reset Request Sent!',
-      detail: 'A reset link has been sent to your email account.'
+      severity: "success",
+      summary: "Password Reset Request Sent!",
+      detail: "A reset link has been sent to your email account.",
     },
     result: response,
   });
-}
+};
 
 /**
  * User password reset
@@ -223,38 +223,35 @@ exports.requestResetPassword = async (req, res, next) => {
 
 exports.resetPassword = async (req, res, next) => {
   try {
-
     // destructure reset token
-    const {userid, token} = req.params;
+    const { userid, token } = req.params;
 
     // find user by ID
     const user = await User.findById(userid);
 
     // check if user is a registered user
-    if (!user) return next(new Error('notFound'));
+    if (!user) return next(new Error("notFound"));
 
     // check if token is valid
     const isValid = await validateToken(userid, token);
-    if (!isValid) return next(new Error('passwordResetFailed'));
+    if (!isValid) return next(new Error("passwordResetFailed"));
 
     // reset password
     const result = await User.resetPassword(req.body);
 
     // delete token
-    await deleteToken(userid)
+    await deleteToken(userid);
 
     res.status(200).json({
       message: {
-        severity: 'success',
-        summary: 'Success',
-        detail: 'Your password has been reset.'
+        severity: "success",
+        summary: "Success",
+        detail: "Your password has been reset.",
       },
       result: !!result.id,
     });
+  } catch (e) {
+    console.error(e);
+    next(new Error("passwordResetFailed"));
   }
-  catch (e) {
-    console.error(e)
-    next(new Error('passwordResetFailed'));
-  }
-}
-
+};
