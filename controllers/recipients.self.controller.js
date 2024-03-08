ac4d4f91a4e62dc4258b0f59a9f10096c3c18c66
DenisKorkut-bcgov <125493524+DenisKorkut-bcgov@users.noreply.github.com>
2024-03-08 11:03:01 -0800

    Remove empty supervisor address sent from front-end for bulk ministry ship cases
    Causes SQL error

--- controllers/recipients.self.controller.js
+++ controllers/recipients.self.controller.js
@@ -6,8 +6,8 @@
  */
 
 const Recipient = require("../models/recipients.model.js");
-const {sendRegistrationConfirmation} = require("../services/mail.services");
-const {confirm} = require("../services/validation.services");
+const { sendRegistrationConfirmation } = require("../services/mail.services");
+const { confirm } = require("../services/validation.services");
 
 /**
  * Retrieve record for current recipient.
@@ -21,16 +21,16 @@ const {confirm} = require("../services/validation.services");
 
 exports.get = async (req, res, next) => {
   try {
-    const {guid=null} = res.locals.user || {};
+    const { guid = null } = res.locals.user || {};
 
     // for delegated users / self-registrations
     const recipient = await Recipient.findByGUID(guid);
-    if (!recipient) return next(Error('noRecord'));
+    if (!recipient) return next(Error("noRecord"));
     res.status(200).json({
       message: {
-        severity: 'success',
-        summary: 'Recipient Record(s) Found',
-        detail: 'Recipient records found.'
+        severity: "success",
+        summary: "Recipient Record(s) Found",
+        detail: "Recipient records found.",
       },
       result: recipient.data,
     });
@@ -49,22 +49,21 @@ exports.get = async (req, res, next) => {
 
 exports.register = async (req, res, next) => {
   try {
-
-    let { guid=null, idir=null } = res.locals.user || {};
+    let { guid = null, idir = null } = res.locals.user || {};
 
     // register recipient (creates stub record)
     await Recipient.register({
       idir: idir,
       guid: guid,
-      status: 'self'
+      status: "self",
     });
     const recipient = await Recipient.findByGUID(guid);
 
     res.status(200).json({
       message: {
-        severity: 'success',
-        summary: 'Add Recipient',
-        detail: 'New recipient record created.'
+        severity: "success",
+        summary: "Add Recipient",
+        detail: "New recipient record created.",
       },
       result: recipient.data,
     });
@@ -84,25 +83,36 @@ exports.register = async (req, res, next) => {
 
 exports.save = async (req, res, next) => {
   try {
-    let { guid=null } = res.locals.user || {};
+    let { guid = null } = res.locals.user || {};
 
     // check that recipient exists
     const recipient = await Recipient.findByGUID(guid);
 
     // handle exceptions
-    if (!recipient) return next(Error('noRecord'));
-    if (recipient.confirmed) return next(Error('alreadySubmitted'));
+    if (!recipient) return next(Error("noRecord"));
+    if (recipient.confirmed) return next(Error("alreadySubmitted"));
 
     // check confirmation status of registration
-    const {service} = req.body || {};
-    const {confirmed} = service || {};
+    const { service } = req.body || {};
+    const { confirmed } = service || {};
 
+    if (
+      req.body.organization &&
+      req.body.organization.bulk &&
+      req.body.supervisor &&
+      req.body.supervisor.office_address
+    ) {
+      console.log(req.body.supervisor);
+      delete req.body.supervisor.office_address;
+      console.log(req.body.supervisor);
+    }
     // Handle registration submissions
     // - check confirmation status of registration
     // - confirm submission data is complete
     if (confirmed) {
       // check that submission has required fields
-      if (!confirm(recipient.schema, req.body)) return next(Error('registrationIncomplete'));
+      if (!confirm(recipient.schema, req.body))
+        return next(Error("registrationIncomplete"));
       // update record
       await recipient.save(req.body);
       // send confirmation email (if confirmed)
@@ -114,9 +124,9 @@ exports.save = async (req, res, next) => {
 
     res.status(200).json({
       message: {
-        severity: 'success',
-        summary: 'Recipient Saved Successfully!',
-        detail: 'Recipient record saved.'
+        severity: "success",
+        summary: "Recipient Saved Successfully!",
+        detail: "Recipient record saved.",
       },
       result: recipient.data,
     });
@@ -136,20 +146,20 @@ exports.save = async (req, res, next) => {
 
 exports.remove = async (req, res, next) => {
   try {
-    let { guid=null } = res.locals.user || {};
+    let { guid = null } = res.locals.user || {};
 
     // check that recipient exists
     const recipient = await Recipient.findByGUID(guid);
-    if (!recipient) return next(Error('noRecord'));
+    if (!recipient) return next(Error("noRecord"));
 
     // delete record
     await recipient.delete();
 
     res.status(200).json({
       message: {
-        severity: 'success',
-        summary: 'Recipient Deleted Successfully!',
-        detail: 'Recipient record deleted.'
+        severity: "success",
+        summary: "Recipient Deleted Successfully!",
+        detail: "Recipient record deleted.",
       },
       result: null,
     });
