0bf1b73220076326d436cd53d8f00a86115a9d00
brysonjbest <103070659+brysonjbest@users.noreply.github.com>
2024-03-11 15:59:17 -0700

    Update awards query to include selection count for current year.

--- queries/awards.queries.js
+++ queries/awards.queries.js
@@ -29,6 +29,9 @@ const awardsQueries = {
     // destructure filter for sort/order/offset/limit
     const { active = true, milestone = null } = filter || {};
 
+    // get current year for query of awards selected in the current year
+    const currentYear = new Date().getFullYear();
+
     // (optional) order by attribute
     // const orderClause = order && orderby ? `ORDER BY ${orderby} ${order}` : '';
     // const limitClause = limit ? `LIMIT ${limit}` : '';
@@ -47,30 +50,33 @@ const awardsQueries = {
 
     return {
       sql: `
-            SELECT * 
+      WITH awd_cycle_filtered AS (SELECT service_selections.id as "service_id" FROM service_selections LEFT JOIN (SELECT * FROM award_selections) as "awdselects" on service_selections.id = awdselects.id WHERE service_selections.cycle = ${currentYear})
+
+      SELECT awds.id, opts_award_id, select_award_id, awds.*, selections.selected, opts.options
             FROM awards AS "awds"
-                    -- award options details
-                     LEFT JOIN (
-                      SELECT awdopts.award as opts_award_id,
-                             JSON_AGG( json_build_object(
-                                 'id', awdopts.id,
-                                 'award', awdopts.award,
-                                 'type', awdopts.type,
-                                 'name', awdopts.name,
-                                 'description', awdopts.description,
-                                 'label', awdopts.label,
-                                 'value', awdopts.value,
-                                 'customizable', awdopts.customizable
-                            )) AS options
-                      FROM "award_options" AS "awdopts"
-                      GROUP BY opts_award_id
-                ) AS "opts" ON opts_award_id = "awds"."id"
-                -- recipient award selections
-                LEFT JOIN (
-                    SELECT awdsel.award as select_award_id, COUNT(*) AS selected
-                    FROM "award_selections" AS "awdsel"
-                    GROUP BY awdsel.award
-                ) AS "selections" ON select_award_id = "awds"."id"  
+            -- award options details
+            LEFT JOIN (
+                SELECT awdopts.award as opts_award_id,
+                       JSON_AGG(json_build_object(
+                           'id', awdopts.id,
+                           'award', awdopts.award,
+                           'type', awdopts.type,
+                           'name', awdopts.name,
+                           'description', awdopts.description,
+                           'label', awdopts.label,
+                           'value', awdopts.value,
+                           'customizable', awdopts.customizable
+                       )) AS options
+                FROM "award_options" AS "awdopts"
+                GROUP BY opts_award_id
+            ) AS "opts" ON opts_award_id = "awds"."id"
+            -- recipient award selections
+            LEFT JOIN (
+                SELECT awdsel.award as select_award_id, COUNT(*) AS selected
+                FROM "award_selections" AS "awdsel"
+                WHERE awdsel.id in (select service_id FROM awd_cycle_filtered)
+                GROUP BY awdsel.award
+            ) AS "selections" ON select_award_id = "awds"."id"
                 ${
                   filterClauses.length > 0
                     ? "WHERE " + filterClauses.join(" AND ")
